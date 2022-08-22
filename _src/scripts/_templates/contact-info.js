9446aee8a94eda1749ab421e009e0c781b891583
Steve McDonald <steve.social@epst.co>
2022-08-22 07:48:16 -0500

    updated SVG processing

--- _src/scripts/_templates/contact-info.js
+++ _src/scripts/_templates/contact-info.js
@@ -12,7 +12,12 @@ export function contactInfo_template( contact_info ){ return `
 <div class="phone">${contact_info.phone}</div>
 <div class="social">
     ${contact_info.social.map(social_media => {
-        return `<a href="${social_media.profile}"><img src="${social_media.icon.val}" alt="${social_media.icon.alt}" ></a>`
-    }).join('')}
+        return `
+            <a href="${social_media.profile}">
+                <svg class="${social_media.icon.class}" alt="${social_media.icon.alt}">
+                    <use href="${social_media.icon.val}"></use>
+                </svg>
+            </a>
+    `}).join('')}
 </div>
 `;}
